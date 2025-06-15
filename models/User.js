04225fc0093fae9b29f874d4067e2af494f2c09e
kmazza-hub <kmazza2006@yahoo.com>
2025-06-15 01:27:10 -0400

    updated:

--- models/User.js
+++ models/User.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email is required'],
     unique: true,
     lowercase: true,
+    trim: true,
   },
   passwordHash: {
     type: String,
-    required: true,
+    required: [true, 'Password hash is required'],
   },
 });
 
 module.exports = mongoose.model('User', userSchema);
+
