36a21af434e991062821bbfdf0d37abd03968c7e
kmazza-hub <kmazza2006@yahoo.com>
2025-06-16 22:29:27 -0400

    updated videos tags

--- models/FavoriteVideo.js
+++ models/FavoriteVideo.js
@@ -1,12 +1,11 @@
-// models/FavoriteVideo.js
 const mongoose = require("mongoose");
 
 const favoriteVideoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   videoUrl: { type: String, required: true },
-  tag: { type: String, default: "Untagged" }, // ✅ Added tag field
+  tag: { type: String, default: "Untagged" }, 
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  addedAt: { type: Date, default: Date.now },
+  addedAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("FavoriteVideo", favoriteVideoSchema);
